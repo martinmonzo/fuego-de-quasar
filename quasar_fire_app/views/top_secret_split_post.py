d9dca977bdac420649c866e4791663ef3f6f95db
Martin Monzo Gancia <mmonzo@eventbrite.com>
2021-03-14 02:54:42 -0300

    Fix views split

---
+++ quasar_fire_app/views/top_secret_split_post.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from quasar_fire_app.serializers.satellite import SatelliteSplitSerializer
+from quasar_fire_app.server.set_distance_and_message import SetDistanceAndMessage
+
+
+class TopSecretSplitPostAPIView(APIView):
+    """POST: Update the distance fromt the transmitter to the satellite."""
+    
+    serializer_class = SatelliteSplitSerializer
+
+    def post(self, request, satellite_name):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            SetDistanceAndMessage(request, satellite_name=satellite_name)
+
+            return Response(status=status.HTTP_200_OK)
+        except APIException as ex:
+            return Response(status=status.HTTP_404_NOT_FOUND)
